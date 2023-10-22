09411a3a235567d41102b0a8b6b66e2444b38629
root <ferdinandzhong@gmail.com>
2023-10-22 21:51:56 +0800

    3080 change

--- scripts/launcher_llama2.py
+++ scripts/launcher_llama2.py
@@ -5,7 +5,7 @@ import os
 import signal
 
 if __name__ == "__main__":
-    config_yaml = "/root/tgi_client/config_yamls/llama2-hf.yaml"
+    config_yaml = "/root/llm_client/config_yamls/llama2-hf.yaml"
     tgi_server_config = TGIServerConfig.parse_obj(read_yaml(config_yaml)["tgi_server_config"])
     proc = start_server(tgi_server_config, "/root/autodl-tmp/logs/llama2.txt")
     try:
--- tgi_client/dataset_processing/bbq_based.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-def load_file_as_df(file_path):
-    raw_df = pd.read_json(path_or_buf=file_path, lines=True)
