e8aa7f7b8d024ea31b2f2f3c7eb7334271040d33
root <ferdinandzhong@gmail.com>
2024-02-02 02:48:10 +0000

    mixtral cn results

--- ray_start_cmd.sh
+++ ray_start_cmd.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
 
-conda activate llm_cultural_values_env
-ray start --head --disable-usage-stats --block --include-dashboard true --dashboard-host 0.0.0.0
+prestart_worker_first_driver=0 ray start --head --disable-usage-stats --include-dashboard true --dashboard-host 0.0.0.0
